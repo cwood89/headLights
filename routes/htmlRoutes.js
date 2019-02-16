a39fdd337503712dc0dd8421a9f277621dd3e8f1
woodtop <cwood89@mac.com>
2019-02-16 16:52:17 -0500

    finished html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,107 +3,150 @@ const axios = require("axios");
 
 module.exports = function (app) {
   app.get("/", (req, res) => {
-
-  })
+    res.render("index")
+  });
 
   app.get("/basketball", (req, res) => {
     axios.get("https://bleacherreport.com/nba/archives").then((response) => {
+      let data = {
+        results: [],
+        sport: "Basketball",
+        image: "/assets/images/basketball.jpg"
+      }
+
 
       const $ = cheerio.load(response.data);
 
-      let results = [];
 
       $("li.even, li.odd").each((i, element) => {
-        results.push({
-          title: $(element).children("h3").text(),
-          link: "https://bleacherreport.com" + $(element).children("h3").children("a").attr("href"),
-          sport: "basketball",
-          hasBeenRead: false
-        });
-      });
-      console.log(results);
-      return res.json(results);
+        data.results.push({
 
-    }).catch((err) => console.log(err))
+          title: $(element).children("h3").text(),
+          preview: $(element).children("p").not(".meta").text(),
+          link: "https://bleacherreport.com" + $(element).children("h3").children("a").attr("href"),
+          sport: "Basketball",
+          hasBeenRead: false
+
+        })
+        console.log(data.results);
+      })
+      res.render("index", data);
+    })
+      .catch((err) => console.log(err))
   });
 
   app.get("/football", (req, res) => {
     axios.get("https://bleacherreport.com/nfl/archives").then((response) => {
+      let data = {
+        results: [],
+        sport: "Football",
+        image: "/assets/images/football.png"
+      }
+
 
       const $ = cheerio.load(response.data);
 
-      let results = [];
 
       $("li.even, li.odd").each((i, element) => {
-        results.push({
+        data.results.push({
+
           title: $(element).children("h3").text(),
+          preview: $(element).children("p").not(".meta").text(),
           link: "https://bleacherreport.com" + $(element).children("h3").children("a").attr("href"),
-          sport: "football",
+          sport: "Football",
           hasBeenRead: false
-        });
-      });
-      console.log(results);
-      return res.json(results);
+
+        })
+        console.log(data.results);
+      })
+      res.render("index", data);
     }).catch((err) => console.log(err))
   });
 
+
   app.get("/baseball", (req, res) => {
     axios.get("https://bleacherreport.com/mlb/archives").then((response) => {
+      let data = {
+        results: [],
+        sport: "Baseball",
+        image: "/assets/images/baseball.jpg"
+      }
+
 
       const $ = cheerio.load(response.data);
 
-      let results = [];
 
       $("li.even, li.odd").each((i, element) => {
-        results.push({
+        data.results.push({
+
           title: $(element).children("h3").text(),
+          preview: $(element).children("p").not(".meta").text(),
           link: "https://bleacherreport.com" + $(element).children("h3").children("a").attr("href"),
-          sport: "baseball",
+          sport: "Baseball",
           hasBeenRead: false
-        });
-      });
-      console.log(results);
-      return res.json(results);
-    }).catch((err) => console.log(err))
+
+        })
+        console.log(data.results);
+      })
+      res.render("index", data);
+    })
+      .catch((err) => console.log(err))
   });
 
   app.get("/soccer", (req, res) => {
     axios.get("https://bleacherreport.com/world-football/archives").then((response) => {
+      let data = {
+        results: [],
+        sport: "Soccer",
+        image: "/assets/images/soccer.jpg"
+      }
+
 
       const $ = cheerio.load(response.data);
 
-      let results = [];
 
       $("li.even, li.odd").each((i, element) => {
-        results.push({
+        data.results.push({
+
           title: $(element).children("h3").text(),
+          preview: $(element).children("p").not(".meta").text(),
           link: "https://bleacherreport.com" + $(element).children("h3").children("a").attr("href"),
-          sport: "soccer",
+          sport: "Soccer",
           hasBeenRead: false
-        });
-      });
-      console.log(results);
-      return res.json(results);
+
+        })
+        console.log(data.results);
+      })
+      res.render("index", data);
     }).catch((err) => console.log(err))
   });
 
   app.get("/hockey", (req, res) => {
     axios.get("https://bleacherreport.com/nhl/archives").then((response) => {
+      let data = {
+        results: [],
+        sport: "Hockey",
+        image: "/assets/images/hockey.jpg"
+      }
+
 
       const $ = cheerio.load(response.data);
 
-      let results = [];
 
       $("li.even, li.odd").each((i, element) => {
-        results.push({
+        data.results.push({
+
           title: $(element).children("h3").text(),
+          preview: $(element).children("p").not(".meta").text(),
           link: "https://bleacherreport.com" + $(element).children("h3").children("a").attr("href"),
-          sport: "hockey",
+          sport: "Hockey",
           hasBeenRead: false
-        });
-      });
-      console.log(results);
-      return res.json(results);
-    }).catch((err) => console.log(err))
+
+        })
+        console.log(data.results);
+      })
+      res.render("index", data);
+    })
+      .catch((err) => console.log(err))
   });
 }
