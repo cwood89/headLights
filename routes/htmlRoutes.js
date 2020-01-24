c273a0bff3375554893daf6964531aa396642046
woodtop <cwood89@mac.com>
2020-01-24 15:11:28 -0500

    fixed all html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,11 +1,8 @@
-const cheerio = require("cheerio");
-const axios = require("axios");
 const mongoose = require("mongoose");
 const Article = require("../models");
 
 
 module.exports = function (app) {
-
 
   app.get("/", (req, res) => {
     let urlArr = [
@@ -29,9 +26,9 @@ module.exports = function (app) {
       image: "/assets/images/baseball.jpg",
     };
 
-    Article.find({})
-      .then(function (dbArticle) {
-        data.results = dbArticle;
+    Article.find({ saved: true })
+      .then(function (articles) {
+        data.results = articles;
         res.render("saved", data);
       })
       .catch((err) => console.log(err));
@@ -47,7 +44,7 @@ module.exports = function (app) {
       image: "/assets/images/basketball.jpg"
     }
 
-    Article.find({ sport: 'Basketball'})
+    Article.find({ sport: 'Basketball' })
       .then(function (articles) {
         data.results = articles;
         res.render('index', data);
@@ -57,117 +54,67 @@ module.exports = function (app) {
 
 
   app.get("/football", (req, res) => {
-    axios.get("https://bleacherreport.com/nfl/archives").then((response) => {
-      let data = {
-        results: [],
-        sport: "Football",
-        image: "/assets/images/football.png"
-      }
+    let data = {
+      results: [],
+      sport: "Football",
+      image: "/assets/images/football.png"
+    }
 
-
-      const $ = cheerio.load(response.data);
-
-
-      $("li.even, li.odd").each((i, element) => {
-        data.results.push({
-
-          title: $(element).children("h3").text(),
-          preview: $(element).children("p").not(".meta").text(),
-          link: "https://bleacherreport.com" + $(element).children("h3").children("a").attr("href"),
-          sport: "Football",
-          hasBeenRead: false
-
-        })
+    Article.find({ sport: 'Football' })
+      .then(function (articles) {
+        data.results = articles;
+        res.render('index', data);
       })
-      res.render("index", data);
-    }).catch((err) => console.log(err))
+      .catch((err) => console.log(err));
   });
 
 
 
   app.get("/baseball", (req, res) => {
-    axios.get("https://bleacherreport.com/mlb/archives").then((response) => {
-      let data = {
-        results: [],
-        sport: "Baseball",
-        image: "/assets/images/baseball.jpg"
-      }
+    let data = {
+      results: [],
+      sport: "Baseball",
+      image: "/assets/images/baseball.jpg"
+    }
 
-
-      const $ = cheerio.load(response.data);
-
-
-      $("li.even, li.odd").each((i, element) => {
-        data.results.push({
-
-          title: $(element).children("h3").text(),
-          preview: $(element).children("p").not(".meta").text(),
-          link: "https://bleacherreport.com" + $(element).children("h3").children("a").attr("href"),
-          sport: "Baseball",
-          hasBeenRead: false
-
-        })
+    Article.find({ sport: 'Baseball' })
+      .then(function (articles) {
+        data.results = articles;
+        res.render('index', data);
       })
-      res.render("index", data);
-    })
-      .catch((err) => console.log(err))
+      .catch((err) => console.log(err));
   });
 
 
 
   app.get("/soccer", (req, res) => {
-    axios.get("https://bleacherreport.com/world-football/archives").then((response) => {
-      let data = {
-        results: [],
-        sport: "Soccer",
-        image: "/assets/images/soccer.jpg"
-      }
-
-      const $ = cheerio.load(response.data);
-
-
-      $("li.even, li.odd").each((i, element) => {
-        data.results.push({
-
-          title: $(element).children("h3").text(),
-          preview: $(element).children("p").not(".meta").text(),
-          link: "https://bleacherreport.com" + $(element).children("h3").children("a").attr("href"),
-          sport: "Soccer",
-          hasBeenRead: false
-
-        })
+    let data = {
+      results: [],
+      sport: "Soccer",
+      image: "/assets/images/soccer.jpg"
+    }
+    
+    Article.find({ sport: 'Soccer' })
+      .then(function (articles) {
+        data.results = articles;
+        res.render('index', data);
       })
-      res.render("index", data);
-    }).catch((err) => console.log(err))
+      .catch((err) => console.log(err));
   });
 
 
 
   app.get("/hockey", (req, res) => {
-    axios.get("https://bleacherreport.com/nhl/archives").then((response) => {
-      let data = {
-        results: [],
-        sport: "Hockey",
-        image: "/assets/images/hockey.jpg"
-      }
-
-
-      const $ = cheerio.load(response.data);
-
-
-      $("li.even, li.odd").each((i, element) => {
-        data.results.push({
-
-          title: $(element).children("h3").text(),
-          preview: $(element).children("p").not(".meta").text(),
-          link: "https://bleacherreport.com" + $(element).children("h3").children("a").attr("href"),
-          sport: "Hockey",
-          hasBeenRead: false
-
-        })
+    let data = {
+      results: [],
+      sport: "Hockey",
+      image: "/assets/images/hockey.jpg"
+    }
+    Article.find({ sport: 'Hockey' })
+      .then(function (articles) {
+        data.results = articles;
+        res.render('index', data);
       })
-      res.render("index", data);
-    })
-      .catch((err) => console.log(err))
+      .catch((err) => console.log(err));
   });
 }
